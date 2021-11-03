0b907b30cab324b1d3704cee0d4d9c588bad9e21
GhassanJar <93609493+GhassanJar@users.noreply.github.com>
2021-11-03 18:11:01 +0200

    3

--- Donut_OpenGL/src/Application.cpp
+++ Donut_OpenGL/src/Application.cpp
@@ -8,14 +8,42 @@ using namespace std;
 static unsigned int CompileShader(unsigned int type, const string& source)
 {
 	unsigned int id = glCreateShader(GL_VERTEX_SHADER);
-	const char* src = source.c_str();
+	const char* src = &source[0];
 	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << endl;
+		cout << message << endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
 }
 
-static int CreateShader(const string& vertexShader, const string& fragmentShader)
+static unsigned int CreateShader(const string& vertexShader, const string& fragmentShader)
 {
 	unsigned int program = glCreateProgram();
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
 }
 
 //this is OpenGL ver.4
@@ -23,7 +51,7 @@ int main(void)
 {
 	GLFWwindow* window;
 
-	cout << " fuck git fucking bloody";
+	cout << " fuck git fucking bloody, sorry mate i changed my mind, FUCK YOU BLOODY";
 
 	//initials
 	if (!glfwInit())
@@ -55,7 +83,27 @@ int main(void)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	string vertexShader =
+		"@version 330 core "
+		"\n"
+		"layout(location = 0) in vec4 position;"
+		"\n"
+		"void main()"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+	string fragmentShader =
+		"@version 330 core "
+		"\n"
+		"layout(location = 0) out vec4 color;"
+		"\n"
+		"void main()"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+
+	unsigned int shader = CreateShader();
+
 
 	//loop
 	while (!glfwWindowShouldClose(window))
