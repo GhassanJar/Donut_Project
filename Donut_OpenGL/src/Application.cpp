a3ade32dc606c9cea98e394203b12e5f60955845
GhassanJar <93609493+GhassanJar@users.noreply.github.com>
2021-11-03 13:00:00 +0200

    1

--- Donut_OpenGL/src/Application.cpp
+++ Donut_OpenGL/src/Application.cpp
@@ -3,6 +3,19 @@
 #include <iostream>
 
 using namespace std;
+
+static unsigned int CompileShader(const string& source, unsigned int type)
+{
+	unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+	const char* src = source.c_str();
+}
+
+static int CreateShader(const string& vertexShader, const string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+}
+
 //this is OpenGL ver.4
 int main(void)
 {
@@ -37,7 +50,10 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);                                  // GL_ARRAY_BUFFER is simply saying that this buffer is an array
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);//we can use sizeof(positions) but it's more practical this way
 
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	//loop
 	while (!glfwWindowShouldClose(window))
@@ -46,7 +62,7 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, positions);
+		/*glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, positions);*/
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
